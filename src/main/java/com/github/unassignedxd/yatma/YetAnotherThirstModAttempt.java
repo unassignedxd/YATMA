f1993613537b2c01c92c307ecd8032403e823854
unassigned <bmhavens6@gmail.com>
2020-10-08 10:39:51 -0600

    initial commit

---
+++ src/main/java/com/github/unassignedxd/yatma/YetAnotherThirstModAttempt.java
@@ -0,0 +1,24 @@
+package com.github.unassignedxd.yatma;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("yatma")
+public class YetAnotherThirstModAttempt {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public YetAnotherThirstModAttempt() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        LOGGER.info("Hello from YATMA");
+    }
+}
